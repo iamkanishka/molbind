9803305fb0fbb953e731cedf095fae8cf07cae21
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-29 21:45:30 +0530

    MoleculeBankComponent

---
+++ src/feature/molecule-bank/molecule-bank.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-molecule-bank',
+  imports: [],
+  templateUrl: './molecule-bank.component.html',
+  styleUrl: './molecule-bank.component.css'
+})
+export class MoleculeBankComponent {
+
+}
