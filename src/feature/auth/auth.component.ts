9bc9d5e29b8b598ac93d14e76c52c1c1c76032c8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-05 21:25:43 +0530

    AuthComponent

--- src/feature/auth/auth.component.ts
+++ src/feature/auth/auth.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+
+import { HttpClient } from '@angular/common/http'
+import { MetamaskAuthService } from 'src/service/auth/metamaskauth.service';
+ 
 
 @Component({
   selector: 'app-auth',
@@ -7,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+
+
+
+  constructor(
+    private metamaskAuth: MetamaskAuthService,
+    private http: HttpClient
+  ) {}
+
+  async login() {
+    try {
+      const address = await this.metamaskAuth.connectWallet();
+
+      // Get nonce from server
+      const  nonce  = await this.http
+        .post<{ nonce: string }>('/api/auth/nonce', { address })
+        .toPromise();
+
+      if (!nonce || !nonce.nonce) {
+        throw new Error('Nonce is undefined or invalid');
+      }
+      const signature = await this.metamaskAuth.signMessage(nonce.nonce);
+
+      // Send signature to backend for verification
+      const result = await this.http
+        .post('/api/auth/verify', { address, signature })
+        .toPromise();
+
+      console.log('Login success', result);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  }
 
 }
