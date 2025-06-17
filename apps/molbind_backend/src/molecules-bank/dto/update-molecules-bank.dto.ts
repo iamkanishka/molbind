543dddd45b2a3a22e78f89709a406e81fc073126
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-17 11:27:58 +0530

    Deleted unused Apps

--- apps/molbind_backend/src/molecules-bank/dto/update-molecules-bank.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMoleculesBankDto } from './create-molecules-bank.dto';
-
-export class UpdateMoleculesBankDto extends PartialType(CreateMoleculesBankDto) {}
