0799804d9d623ba7e1385da8fbd0ca9b24ba87d9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-19 16:16:18 +0530

    feat: Added profile module

--- apps/molbind-api/src/app/app.module.ts
+++ apps/molbind-api/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '@molbindapi/auth'; // Adjust the import path as necessary
+import { ProfileModule } from '@molbindapi/profile'; // Adjust the import path as necessary
+// Note: The import path for AuthModule should match the actual path in your project structure
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
