ef1dee44806ee51b312de2c3b2bf1713070820f1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-19 16:57:50 +0530

    Added chat-ui library

---
+++ libs/molbind/chat-ui/src/lib/chat-ui/chat-ui.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-chat-ui',
+  imports: [CommonModule],
+  templateUrl: './chat-ui.html',
+  styleUrl: './chat-ui.css',
+})
+export class ChatUi {}
