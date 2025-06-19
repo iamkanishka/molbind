633bd09468bda2800b0302d420e6badd6f683bdd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-19 16:16:01 +0530

    feat: Added auth-ui component

--- apps/molbind/src/app/app.ts
+++ apps/molbind/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { AuthUi } from '@molbind/auth-ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, AuthUi],
+  selector: 'app-auth-ui',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
