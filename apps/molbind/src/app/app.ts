7306fddd11bcb106c24b05103b12ce7bb2bce94b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-19 16:23:30 +0530

    chore: Resolved minor isssue

--- apps/molbind/src/app/app.ts
+++ apps/molbind/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AuthUi } from '@molbind/auth-ui';
+import {AuthUi} from '@molbind/auth-ui';
+ 
 
 @Component({
-  imports: [RouterModule, AuthUi],
-  selector: 'app-auth-ui',
+  imports: [RouterModule,AuthUi],
+  selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
